3e73af5ff21988cd6813bb2b5db252bb891783d3
ravikumarhugar17-source <ravikumarhugar17@gmail.com>
2026-04-08 00:19:43 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, a = 0, b = 1, next;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    for(int i = 1; i <= n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
